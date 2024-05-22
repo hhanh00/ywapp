918e41e5fce2978c13735a79089b207293fcb897
Hanh <hanh425@gmail.com>
2024-05-22 10:30:08 +1000

    Remove Nextra from template

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,9 +1,17 @@
 export default {
     logo: <span>YWallet</span>,
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s - YWallet'
+        }
+    },
     editLink: {
         component: null
     },
     feedback: {
         content: null
+    },
+    footer: {
+        text: null
     }
 }
